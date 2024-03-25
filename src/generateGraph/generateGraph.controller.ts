2c87800903fcb59243eaefa6b08baaf639c4ce2f
bebroch <cawapog@yandex.ru>
2024-03-25 14:01:30 +0300

    init: my dev

--- src/generateGraph/generateGraph.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Post } from '@nestjs/common';
-
-@Controller('generate')
-export class GenerateGraphController {
-  @Post()
-  addGenerated(): any {}
-}
--- src/generateGraph/generateGraph.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Graph } from './generateGraph.model';
-@Injectable()
-export class GenerateGraphService {
-  graph: Graph[] = [];
-  insertGraph(name: string, grate: number) {
-    const newGraph = new Graph(new Date().toString(), name, great);
-  }
-}
