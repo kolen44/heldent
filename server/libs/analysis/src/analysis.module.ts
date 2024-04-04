14e177f61194f1ad34cbcf72a51fa9102ebfddff
bebroch <cawapog@yandex.ru>
2024-04-05 01:54:29 +0300

    init: statistic calculation service

--- server/libs/analysis/src/analysis.module.ts
+++ server/libs/analysis/src/analysis.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnalysisService } from './analysis.service';
 import { GraphPredictModule } from './graph-predict/graph-predict.module';
+import { StatisticCalculationModule } from './statistic-calculation/statistic-calculation.module';
 
 @Module({
 	providers: [AnalysisService],
 	exports: [AnalysisService],
-	imports: [GraphPredictModule],
+	imports: [GraphPredictModule, StatisticCalculationModule],
 })
 export class AnalysisModule {}
